06c3bf53137d0bff545116e8683b81895d6d24cd
alanbobs999 <alan999@foxmail.com>
2021-10-07 13:20:19 +0800

    Update dev utils

--- sub/list_update.py
+++ sub/list_update.py
@@ -3,5 +3,5 @@
 import time
 import json
 
-def update_id_0(): # remarks: pojiezhiyuanjun/freev2, 将链接更新至 https://raw.fastgit.org/pojiezhiyuanjun/freev2/master/%MM%(DD - 1).txt
+#def update_id_0(): # remarks: pojiezhiyuanjun/freev2, 将链接更新至 https://raw.fastgit.org/pojiezhiyuanjun/freev2/master/%MM%(DD - 1).txt
 
