07b225c788673057f0395c6058ef3fc755dd0c45
Alan Bobs <amna25499@gmail.com>
2022-07-07 17:52:53 +0800

    Update sub lists

--- utils/list_update.py
+++ utils/list_update.py
@@ -65,12 +65,12 @@ class update_url():
             this_month = datetime.today().strftime('%Y%m')
             url_front = 'https://nodefree.org/dy/'
             url_end = '.txt'
-            new_url = url_front + today + url_end
+            new_url = url_front + this_month + '/' + today + url_end
         if id == 0:
             today = datetime.today().strftime('%m%d')
             url_front = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/'
             url_end = '.txt'
-            new_url = url_front + this_month + '/' + today + url_end
+            new_url = url_front + today + url_end
 
         if url_updated(new_url):
             return new_url
