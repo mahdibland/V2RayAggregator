4f137c38687da4e9ad9b27b788671a85def62ffc
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-09-29 20:24:50 +0330

    Update speedtest.sh

--- utils/speedtest/speedtest.sh
+++ utils/speedtest/speedtest.sh
@@ -1,7 +1,7 @@
 #准备好所需文件
 wget -O clash.gz https://github.com/Dreamacro/clash/releases/download/v1.11.4/clash-linux-amd64-v1.11.4.gz
 gunzip clash.gz
-wget -O lite.gz https://github.com/alanbobs999/LiteSpeedTest/releases/download/v0.11.2m/lite-linux-amd64-v0.11.2m.gz
+wget -O lite.gz https://github.com/mahdibland/LiteSpeedTest/releases/download/v0.11.2/lite-linux-amd64-v0.11.2.gz
 gunzip lite.gz
 wget -O clash_config.yml https://raw.githubusercontent.com/mahdibland/TopFreeProxies/master/utils/speedtest/clash_config.yml
 wget -O proxychains.conf https://raw.githubusercontent.com/mahdibland/TopFreeProxies/master/utils/speedtest/proxychains.conf
