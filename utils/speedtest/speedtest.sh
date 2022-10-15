df5760094fa5f6764cb69769024843fdc0c279e1
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-10-15 12:27:33 +0330

    Update speedtest.sh

--- utils/speedtest/speedtest.sh
+++ utils/speedtest/speedtest.sh
@@ -17,4 +17,4 @@ sudo pkill -f clash
 ./clash -f clash_config.yml &
 #运行 LiteSpeedTest
 chmod +x ./lite
-sudo nohup proxychains ./lite --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml >speedtest.log 2>&1 &
+sudo nohup proxychains ./lite --config lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml >speedtest.log 2>&1 &
