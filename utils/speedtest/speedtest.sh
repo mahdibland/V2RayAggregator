798363310d59b143eb2febdd7ff4fef45114e56a
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-10-18 11:10:55 +0330

    Update speedtest.sh

--- utils/speedtest/speedtest.sh
+++ utils/speedtest/speedtest.sh
@@ -1,8 +1,7 @@
 #准备好所需文件
 wget -O clash.gz https://github.com/Dreamacro/clash/releases/download/v1.11.8/clash-linux-amd64-v1.11.8.gz
 gunzip clash.gz
-wget -O lite.gz https://github.com/alanbobs999/LiteSpeedTest/releases/download/v0.11.2m/lite-linux-amd64-v0.11.2m.gz
-gunzip lite.gz
+wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
 wget -O clash_config.yml https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/clash_config.yml
 wget -O proxychains.conf https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/proxychains.conf
 wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/lite_config.json
@@ -16,5 +15,5 @@ mv -f proxychains.conf ../../../../../etc/proxychains.conf
 sudo pkill -f clash
 ./clash -f clash_config.yml &
 #运行 LiteSpeedTest
-chmod +x ./lite
-sudo nohup proxychains ./lite --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml >speedtest.log 2>&1 &
+chmod +x ./lite-linux-amd64
+sudo nohup proxychains ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml >speedtest.log 2>&1 &
