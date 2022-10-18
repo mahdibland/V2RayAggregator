c3e03f1600f147846f76b4591adace74d605229c
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-10-18 12:18:17 +0330

    Create speedtest2.sh

---
+++ utils/speedtest/speedtest2.sh
@@ -0,0 +1,8 @@
+wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
+wget -O clash_config.yml https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/clash_config.yml
+wget -O proxychains.conf https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/proxychains.conf
+wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/lite_config.json
+###################
+#运行 LiteSpeedTest
+chmod +x ./lite-linux-amd64
+sudo ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml >speedtest.log 2>&1 &
