c1bac31d1d5a2f02fadad10d86fe734b0f25eb8c
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-10-18 13:08:19 +0330

    Update speedtest2.sh

--- utils/speedtest/speedtest2.sh
+++ utils/speedtest/speedtest2.sh
@@ -1,7 +1,7 @@
-wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-
+wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
 wget -O proxychains.conf https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/proxychains.conf
 wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/lite_config.json
 ###################
 #运行 LiteSpeedTest
 chmod +x ./lite-linux-amd64
-sudo ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity >speedtest.log 2>&1 &
+sudo nohub ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity >speedtest.log 2>&1 &
