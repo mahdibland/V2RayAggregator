b72f40b5de5b2e08af8ffc270cd36d8a1a2b17d0
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-10-22 12:17:11 +0330

    fix bug

--- utils/speedtest/speedtest2_yaml_clash.sh
+++ utils/speedtest/speedtest2_yaml_clash.sh
@@ -1,21 +1,19 @@
 # Clash
 wget -O clash.gz https://github.com/Dreamacro/clash/releases/download/v1.11.8/clash-linux-amd64-v1.11.8.gz
 gunzip clash.gz
-wget -O clash_config.yml https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/clash_config_eu.yml
-# Lite Speed
 wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
-wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/SSAggregator/master/utils/speedtest/lite_config_yaml.json
-# Proxy Chains
+wget -O clash_config.yml https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/clash_config_eu.yml
 wget -O proxychains.conf https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/proxychains.conf
-# Setup
+wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/SSAggregator/master/utils/speedtest/lite_config_yaml.json
+#初始化 Clash
 chmod +x ./clash && ./clash &
-#
+#安装并配置 proxychains
 sudo apt-get install proxychains
 sudo chmod 777 ../../../../../../etc/proxychains.conf
 mv -f proxychains.conf ../../../../../../etc/proxychains.conf
-#
+#开始运行 Clash
 sudo pkill -f clash
 ./clash -f clash_config.yml &
-# LiteSpeedTest
+#运行 LiteSpeedTest
 chmod +x ./lite-linux-amd64
 sudo nohup proxychains ./lite-linux-amd64 --config ./lite_config.json --test subs >speedtest.log 2>&1 &
