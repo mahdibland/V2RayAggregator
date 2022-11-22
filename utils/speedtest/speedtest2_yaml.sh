38b3e1658216911aaec08d956aaa081972edc39c
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-11-22 17:40:10 +0330

    🧩 Update Speedtest script

--- utils/speedtest/speedtest2_yaml.sh
+++ utils/speedtest/speedtest2_yaml.sh
@@ -1,6 +1,6 @@
 #准备好所需文件
-wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
-wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/SSAggregator/master/utils/speedtest/lite_config_yaml.json
+wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64-12
+wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/lite_config_yaml.json
 #运行 LiteSpeedTest
 chmod +x ./lite-linux-amd64
 sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test subs >speedtest.log 2>&1 &
