bab727df86d56832af4f0bac08ed9f9e1fb721c0
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-11-22 21:08:23 +0330

    🐟 Fix Issue

--- utils/airport/speedtest/speedtest2_yamlc.sh
+++ utils/airport/speedtest/speedtest2_yamlc.sh
@@ -1,8 +1,8 @@
 # Clash
 wget -O clash.gz https://github.com/Dreamacro/clash/releases/download/v1.11.8/clash-linux-amd64-v1.11.8.gz
 gunzip clash.gz
-wget -O lite-linux-amd64.zip https://github.com/xxf098/LiteSpeedTest/releases/download/v0.13.1/lite-linux-amd64-v0.13.1.gz
-gzip -d lite-linux-amd64.zip
+wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.13.1/lite-linux-amd64-v0.13.1.gz
+gzip -d lite-linux-amd64.gz
 wget -O clash_config.yml https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/clash_config_eu.yml
 wget -O proxychains.conf https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/proxychains.conf
 wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/airport/speedtest/lite_config_yaml_airport.json
