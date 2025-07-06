93f3a96da1ebbb202b17c67618e05ac7e16e675a
Mehrdad Oliaee <mehr1dado@gmail.com>
2025-07-06 08:50:35 +0330

    Delete utils directory

--- utils/airport/speedtest/speedtest2_yaml.sh
+++
@@ -1,7 +0,0 @@
-#准备好所需文件
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64-v0.15.0.gz
-gzip -d lite-linux-amd64.gz
-wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/airport/speedtest/lite_config_yaml_airport.json
-#运行 LiteSpeedTest
-chmod +x ./lite-linux-amd64
-sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/airport_merge_yaml.yml > speedtest.log 2>&1 &
--- utils/speedtest/speedtest2_yaml.sh
+++
@@ -1,7 +0,0 @@
-#准备好所需文件
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
-gzip -d lite-linux-amd64.gz
-wget -O lite_config.json https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/utils/speedtest/lite_config_yaml.json
-#运行 LiteSpeedTest
-chmod +x ./lite-linux-amd64
-sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge_yaml.yml > speedtest.log 2>&1 &
